70bcde1e57beea9ae9b41fa2fd26a1108f429695
MuhammadJalilAsyakur <jalilasyakur@gmail.com>
2023-10-16 13:03:34 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+function App() {
+
+  return (
+    <>
+    <header>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+
+    <main>
+      <section></section>
+    </main>
+
+    <footer>
+      <section></section>
+    </footer>
+    </>
+  )
+}
+
+export default App
